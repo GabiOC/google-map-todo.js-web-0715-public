a5ccf8db65c57dbc9444449d3e7ce5c3b1f99f28
GabiOC <oconnor.gabriele@gmail.com>
2015-09-11 16:35:29 -0400

    all done

--- public/javascripts/fetch_location.js
+++ public/javascripts/fetch_location.js
@@ -2,8 +2,12 @@ $(document).ready(function(){
 
   function getLocation() {
     // code here
+    navigator.geolocation.getCurrentPosition(function(position) {
+      $("#latitude").text(position.coords.latitude);
+      $("#longitude").text(position.coords.longitude);
+    });
   }
 
   getLocation();
-
+  insertMap($("#latitude").text(), $("#longitude").text());
 });
