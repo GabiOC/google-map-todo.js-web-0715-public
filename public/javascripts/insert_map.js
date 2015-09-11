aed2e469c543b20f400ffb13ec2ad13ac259c5dd
GabiOC <oconnor.gabriele@gmail.com>
2015-09-11 18:09:53 -0400

    all done

--- public/javascripts/insert_map.js
+++ public/javascripts/insert_map.js
@@ -11,14 +11,3 @@ function insertMap(latitude, longitude) {
   }
   google.maps.event.addDomListener(window, 'load', initialize);
 }
-//
-// function initialize() {
-// +    var mapCanvas = document.getElementById('map');
-// +    var mapOptions = {
-// +      center: new google.maps.LatLng(latitude, longitude),
-// +      zoom: 8,
-// +      mapTypeId: google.maps.MapTypeId.ROADMAP
-// +    };
-// +    var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-// +  }
-// +  google.maps.event.addDomListener(window, 'load', initialize);
